9ba89f9cd42f7f6b15a2dc0c6dad6a0c1b90cc72
Bogdan_Detrov <vernitynout@gmail.com>
2020-05-03 18:46:28 +1000

    На всякий случай v 2.0

--- python_org_news.py
+++ python_org_news.py
@@ -10,23 +10,20 @@ def get_html(url):
         print("Сетевая ошибка")
         return False
 
-def get_python_news(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
-    result_news = []
-    for news in all_news:
-        title = news.find('a').text
-        url = news.find('a')['href']
-        published = news.find('time').text
-        result_news.append({
-            "title": title,
-            "url": url,
-            "published": published
-        })
-    return result_news
-
-if __name__ == "__main__":
+def get_python_news():
     html = get_html("https://www.python.org/blogs/")
     if html:
-        news = get_python_news(html)
-        print(news)
+        soup = BeautifulSoup(html, 'html.parser')
+        all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
+        result_news = []
+        for news in all_news:
+            title = news.find('a').text
+            url = news.find('a')['href']
+            published = news.find('time').text
+            result_news.append({
+                "title": title,
+                "url": url,
+                "published": published
+            })
+        return result_news
+    return False 
--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
+
+from python_org_news import get_python_news
 from weather import weather_by_city
-from news_python import get_python_news
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    title = 'Новости Python'
+def index():
+    title = "Новости Python"
     weather = weather_by_city("Vladivostok")
-    news_list = get_python_news()
-    return render_template("index.html", page_title=title, weather=weather, news=news_list)
+    return render_template('index.html', page_title=title, weather=weather)
 
 if __name__ == "__main__":
     app.run(debug=True)
