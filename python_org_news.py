4964789966b4259811b89df81c0ab66c18dc2ea1
Bogdan_Detrov <vernitynout@gmail.com>
2020-05-03 18:36:27 +1000

    На всякий случай, а то я это уже переписывал)

--- python_org_news.py
+++ python_org_news.py
@@ -12,10 +12,21 @@ def get_html(url):
 
 def get_python_news(html):
     soup = BeautifulSoup(html, 'html.parser')
-    all_news = soup.find('ul', class_='list-recent-posts')
-    print(all_news)
+    all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
+    result_news = []
+    for news in all_news:
+        title = news.find('a').text
+        url = news.find('a')['href']
+        published = news.find('time').text
+        result_news.append({
+            "title": title,
+            "url": url,
+            "published": published
+        })
+    return result_news
 
 if __name__ == "__main__":
     html = get_html("https://www.python.org/blogs/")
     if html:
-        get_python_news(html)
+        news = get_python_news(html)
+        print(news)
