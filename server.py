9e28be9def2c0a092e9da35f9e25a762c30b6229
Bogdan_Detrov <vernitynout@gmail.com>
2020-05-03 18:23:52 +1000

    Продолжаем делать сайтик

--- server.py
+++ server.py
@@ -1,27 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
+from news_python import get_python_news
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
+    title = 'Новости Python'
     weather = weather_by_city("Vladivostok")
-    if weather:
-        weather_text = f"Погода: {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}."
-    else:
-        weather_text = 'Сервис погоды временно недоступен.'
-    return f"""
-    <html>
-        <head>
-            <title>Прогноз погоды</title>
-        </head>
-        <body>
-            <h1>{weather_text}</h1>
-        </body>
-    </html>
-    """
-
+    news_list = get_python_news()
+    return render_template("index.html", page_title=title, weather=weather, news=news_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
